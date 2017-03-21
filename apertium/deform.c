577e2cd3aefe9c33910740147faced35f64a4881
hrishi1995 <paramane1995hrishi@gmail.com>
2017-03-21 20:08:10 +0530

    Working on deformatting using libxml2

--- apertium/deform.c
+++ apertium/deform.c
@@ -3,66 +3,98 @@
 #include <stdlib.h>
 #include <libxml/HTMLparser.h>
 
+char *inline_tags[] = {"i", "u", "b"};
+
+int i_tag = sizeof(inline_tags)/sizeof(inline_tags[0]);
+
+char *block_tags[] = {"div", "p", "h1", "h2", "h3", "h4", "h5", "h6"};
+
+int b_tag = sizeof(block_tags)/sizeof(block_tags[0]);
+
+
+
 void apert_format(xmlNode * a_node)
 {
-    xmlNode *cur_node = NULL;
+	xmlNode *cur_node = NULL;
+	int flag=0,i=0;
 
-    if(NULL == a_node)
-    {
-        //printf("Invalid argument a_node %p\n", a_node);
-        return;
-    }
+	int max_len;
 
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) 
-    {
-        if (cur_node->type == XML_ELEMENT_NODE) 
-        {
-            /* Check for if current node should be exclude or not */
-            printf("[{%s}]", cur_node->name);
-        }
-        else if(cur_node->type == XML_TEXT_NODE)
-        {
-            /* Process here text node, It is available in cpStr :TODO: */
-            printf("%s", (char *)cur_node->content);
-        }
-        apert_format(cur_node->children);
-    }
+	if(i_tag>b_tag)
+	{
+		max_len=i_tag;
+	}
+	else
+	{
+		max_len=b_tag;
+	}    	
+
+	if(NULL == a_node)
+	{
+        	return;
+    	}
+
+	for (cur_node = a_node; cur_node; cur_node = cur_node->next) 
+    	{
+        	if (cur_node->type == XML_ELEMENT_NODE) 
+        	{
+			
+			for(i = 0; i <3; ++i)
+			{
+    				if(!(strcmp(inline_tags[i],cur_node->name)))
+    				{
+					flag=1;
+					printf("[{<%s>}]", cur_node->name);
+					break;
+    				}
+			}
+			if(flag!=1)
+			{
+				printf("[<%s>]", cur_node->name);
+			}
+            		//printf("[{%s}]", cur_node->name);
+        	}
+        	else if(cur_node->type == XML_TEXT_NODE)
+        	{
+            		printf("%s", (char *)cur_node->content);
+        	}
+        	apert_format(cur_node->children);
+    	}
 }
 
 int main(int argc, char **argv) 
 {
-    htmlDocPtr doc;
-    xmlNode *roo_element = NULL;
+	htmlDocPtr doc;
+	xmlNode *roo_element = NULL;
+    	if (argc != 2)  
+    	{
+        	printf("\nInvalid argument\n");
+        	return(1);
+    	}
 
-    if (argc != 2)  
-    {
-        printf("\nInvalid argument\n");
-        return(1);
-    }
+    	
+    	LIBXML_TEST_VERSION    
 
-    /* Macro to check API for match with the DLL we are using */
-    LIBXML_TEST_VERSION    
+    	doc = htmlReadFile(argv[1], NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+    	if (doc == NULL) 
+    	{
+        	fprintf(stderr, "Document not parsed successfully.\n");
+        	return 0;
+    	}
 
-    doc = htmlReadFile(argv[1], NULL, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
-    if (doc == NULL) 
-    {
-        fprintf(stderr, "Document not parsed successfully.\n");
-        return 0;
-    }
+    	roo_element = xmlDocGetRootElement(doc);
 
-    roo_element = xmlDocGetRootElement(doc);
+    	if (roo_element == NULL) 
+    	{
+        	fprintf(stderr, "empty document\n");
+        	xmlFreeDoc(doc);
+        	return 0;
+    	}
+    	
+	
+	apert_format(roo_element);
 
-    if (roo_element == NULL) 
-    {
-        fprintf(stderr, "empty document\n");
-        xmlFreeDoc(doc);
-        return 0;
-    }
-
-    printf("Root Node is %s\n", roo_element->name);
-    apert_format(roo_element);
-
-    xmlFreeDoc(doc);       // free document
-    xmlCleanupParser();    // Free globals
-    return 0;
+    	xmlFreeDoc(doc);       // free document
+    	xmlCleanupParser();    // Free globals
+    	return 0;
 }
